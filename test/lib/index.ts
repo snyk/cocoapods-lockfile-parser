567654fa566d3f802073c0634a7fe32c46a08ae5
Marius Rackwitz <git@mariusrackwitz.de>
2019-08-01 11:33:59 +0100

    style: initial format with prettier

--- test/lib/index.ts
+++ test/lib/index.ts
@@ -3,7 +3,7 @@
 // See: https://github.com/tapjs/node-tap/issues/313#issuecomment-250067741
 // tslint:disable:max-line-length
 // tslint:disable:object-literal-key-quotes
-import {test} from 'tap';
+import { test } from 'tap';
 import fun from '../../lib';
 
 test('Is everything ready for the development?', async (t) => {
